61fc93185d4b9d8372461bd50afaaaa7c7f36bab
Paulino <paulinomendes03@gmail.com>
2024-11-17 01:59:58 -0300

    refactor(Relatorio): utiliza ModelAndView no controller e corrige exibição das categorias na view

--- src/main/java/br/edu/fateczl/controle_estoque/controller/RelatorioController.java
+++ src/main/java/br/edu/fateczl/controle_estoque/controller/RelatorioController.java
@@ -2,29 +2,31 @@ package br.edu.fateczl.controle_estoque.controller;
 
 import java.util.List;
 
+import br.edu.fateczl.controle_estoque.service.CategoriaService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import br.edu.fateczl.controle_estoque.service.RelatorioService;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/relatorios")
 public class RelatorioController {
 
-    private RelatorioService relatorioService;
+    private final RelatorioService relatorioService;
+    private final CategoriaService categoriaService;
 
-    public RelatorioController(RelatorioService relatorioService) {
+    public RelatorioController(RelatorioService relatorioService, CategoriaService categoriaService) {
         this.relatorioService = relatorioService;
+        this.categoriaService = categoriaService;
     }
 
     @GetMapping
-    public String exibirFormularioRelatorio(
-            @RequestParam(value = "categoriaId", required = false) Long categoriaId,
-            @RequestParam(value = "status", required = false) Boolean status,
-            Model model) {
+    public ModelAndView exibirFormularioRelatorio(
+            @RequestParam(value = "categoriaId", required = false, defaultValue = "1") Long categoriaId,
+            @RequestParam(value = "status", required = false, defaultValue = "true") Boolean status) {
 
         // Lista que irá armazenar o relatório
         List<Object[]> relatorio = null;
@@ -36,10 +38,12 @@ public class RelatorioController {
         }
 
         // Adiciona os atributos necessários no modelo para a página
-        model.addAttribute("relatorio", relatorio);
-        model.addAttribute("categoriaId", categoriaId);
-        model.addAttribute("status", status);
+        ModelAndView mv = new ModelAndView("relatorio/formularioRelatorio");
+        mv.addObject("relatorio", relatorio);
+        mv.addObject("categoriaId", categoriaId);
+        mv.addObject("status", status);
+        mv.addObject("categorias", categoriaService.todasCategorias());
 
-        return "relatorio/formularioRelatorio"; // Página com o formulário e relatório
+        return mv; // Página com o formulário e relatório
     }
 }
